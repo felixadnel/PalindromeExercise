02e0a1eb12a9daf8a9bc2adef2f7c3c82bb399b6
Felix Adnel <felixadnel@Felixs-MacBook-Pro.local>
2024-02-28 07:33:06 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			
+			var reversed = "";
+
+			for (int i = word.Length -1; i >= 0; i--)
+			{
+				reversed += word[i]; 
+			}
+
+		
+			if(reversed.ToLower() == word.ToLower())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+			
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("radar",true)]
+        [InlineData("hi",false)]
+        [InlineData("Radar",true)]
+        public void MyTest(string word, bool expected)
         {
-
+           
+            var tester = new WordSmith();
+             
+            var actual = tester.IsAPalindrome(word);
+         
+            Assert.Equal(expected, actual);
         }
     }
 }
